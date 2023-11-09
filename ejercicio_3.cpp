ea53795c95d5f7efe1073da05cab3afe66d52f7f
Javier1609 <jvrgmz@myuax.com>
2023-11-09 12:29:49 +0100

    creo la funcion int main donde muestro los valores iniciales y despues llamo a la funcion para el cambio de valores.

--- ejercicio_3.cpp
+++ ejercicio_3.cpp
@@ -5,3 +5,16 @@ void intercambiar(int* A, int* B) {
     *A = *B;
 }
 
+int main() {
+    int a = 5;
+    int b = 10;
+
+    std::cout << "Valores iniciales: a = " << a << ", b = " << b << std::endl;
+
+
+    intercambiar(&a, &b);
+
+    std::cout << "Valores intercambiados: a = " << a << ", b = " << b << std::endl;
+
+    return 0;
+}
