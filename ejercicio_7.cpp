4182f5c7a9b8d23f7cf05a8a8ef2fc0875fb779d
Javier1609 <jvrgmz@myuax.com>
2023-11-09 13:40:37 +0100

    creamos la clase asistencia que tiene dos variables uno la fecha y otro el estado, despues el constructor de la clase asistencia y por ultimo que nos muestre la asistencia.

---
+++ ejercicio_7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+class Asistencia {
+private:
+    std::string fecha;
+    std::string estado;
+
+public:
+
+    Asistencia(const std::string& fecha, const std::string& estado) {
+        this->fecha = fecha;
+        this->estado = estado;
+    }
+
+
+    void mostrar_asistencia() {
+        std::cout << "Fecha de la clase: " << fecha << std::endl;
+        std::cout << "Estado de asistencia: " << estado << std::endl;
+    }
+};
+
+
+
+
+
