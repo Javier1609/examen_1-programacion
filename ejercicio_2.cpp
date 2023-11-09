b63869cb1ed07ed2f29ab1da7d9d3b06d7cd323e
Javier1609 <jvrgmz@myuax.com>
2023-11-09 11:57:34 +0100

    asigna los valores de nombre, edad y grado directamente a las propiedades de la clase

--- ejercicio_2.cpp
+++ ejercicio_2.cpp
@@ -2,11 +2,17 @@
 #include <string>
 
 class Estudiante {
-    int edad;
+private:
     std::string nombre;
+    int edad;
     std::string grado;
 
-}
+public:
 
+    Estudiante(const std::string& n, int e, const std::string& g) {
+        nombre = n;
+        edad = e;
+        grado = g;
+    }
 
 
